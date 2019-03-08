91c25d74e590ebf797409d4cdc2b9ce80109a5c7
pac9 <pac9@btinternet.com>
2019-03-08 20:58:08 +0000

    Set up homepage structure

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Forecast Home</title>
+</head>
+
+<!--- Main body --->
+<body>
+  <header>
+
+
+  </header>
+<main>
+
+
+</main>
+
+<footer>
+
+    &copy; 2019 Pauline Cairns
+</footer>
+
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pauline
+ * Date: 2019-03-08
+ * Time: 20:49
+ */
+?>
