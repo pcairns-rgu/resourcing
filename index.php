e1a44b021d2cdc1b02f643a91660d8ddf35cfc9e
Pauline <p.cairns@rgu.ac.uk>
2019-03-09 21:22:08 +0000

    put bootstrap into homepage

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+    shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <title>Forecast Home</title>
     <link rel="stylesheet" href="assets/css/homepage.css">
     <link rel="stylesheet" href="assets/css/style.css">
@@ -9,6 +14,7 @@
 
 <!--- Main body --->
 <body>
+<div class="container">
   <header>
  <p id="login">Log-in</p>
 
@@ -42,6 +48,10 @@
             </ul>
     </div>
 </footer>
+</div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 </body>
 </html>
