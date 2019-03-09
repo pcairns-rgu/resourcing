df11ce11ab60af4bb813b41f402bf89bb833d8d8
Pauline <p.cairns@rgu.ac.uk>
2019-03-09 11:11:51 +0000

    Initial work on set up of footer of homepage

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Forecast Home</title>
+    <link rel="stylesheet" href="assets/css/homepage.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
 </head>
 
 <!--- Main body --->
@@ -12,13 +15,30 @@
 
   </header>
 <main>
-
+    <h1>FORECAST</h1>
+    <h3>COMBINING TASK MANAGEMENT WITH RESOURCING</h3>
 
 </main>
-
+<hr />
 <footer>
+    <div class="connect">
+    <p>FORECAST - FORESEE ORGANISE RESOURCE EFFICIENT COLLABORATE ACHIEVE SHARE TRANSFER </p>
+    <p>Connect with us:</p>
+    <p>Facebook</p>
+    <p>Twitter</p>
+    <p>LinkedIn</p>
+    <p>Instagram</p>
+    <p>You Tube</p>
+    </div>
 
-    &copy; 2019 Pauline Cairns
+    <div class="bottom">
+   <p>&copy; 2019 Pauline Cairns</p>
+        <p>Contact details</p>
+        <p>T&Cs</p>
+        <p>About</p>
+        <p>Cookies</p>
+
+    </div>
 </footer>
 
 </body>
