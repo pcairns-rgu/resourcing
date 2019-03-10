1e1a13cb9de4aa9963fd6d3eba3285ac8f105c9c
Pauline <p.cairns@rgu.ac.uk>
2019-03-10 17:28:28 +0000

    Add contact us symbols to contact page

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1,
     shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <title>Forecast Home</title>
+
     <link rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>Forecast Home</title>
     <link rel="stylesheet" href="assets/css/homepage.css">
+    <link rel="stylesheet" href="assets/css/colours.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 
