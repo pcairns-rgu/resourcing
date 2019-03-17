78034d2333a139b82270a038ba3abe179c89a6a5
Pauline <p.cairns@rgu.ac.uk>
2019-03-17 12:17:13 +0000

    Set up input_private_task to post private task to DB

--- sql/private.sql
+++ sql/private.sql
@@ -1,9 +1,11 @@
+DROP TABLE private;
+
 CREATE TABLE private(
-                     id int(7) NOT NULL AUTO_INCREMENT,
-                     today DATE,
-                     task varchar(250),
-                     comments varchar(250),
-                     deadline date,
-                     completed bit,
-                     PRIMARY KEY(id)
+                      id int(7) NOT NULL AUTO_INCREMENT,
+                      today DATE,
+                      task varchar(250),
+                      comments varchar(250),
+                      deadline date,
+                      completed bit,
+                      PRIMARY KEY(id)
 )
