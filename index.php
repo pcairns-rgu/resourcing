cbba36a502cceb4096ec81ff9100b389df9776c6
Pauline <p.cairns@rgu.ac.uk>
2019-03-31 09:39:11 +0100

    Tidy up formatting of code on index.html and homepage.css along with minor code changes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1,
     shrink-to-fit=no">
@@ -11,73 +12,79 @@
     <link rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/homepage.css">
-
     <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
 
 <!--- Main body --->
 <body>
-<div class="container colour">
 
+  <div class="container colour">
 
+<!--Start of header -->
   <header>
-
+   <!-- Top nav bar -->
       <nav class="navbar navbar-expand-sm right">
           <ul class="navbar-nav">
       <li class="nav-item"><a href="login_form.php">Log-in</a></li>
           </ul>
       </nav>
+
   </header>
-<main class="middle">
-    <div id="banner">
+<!--- End of header -->
+
+<!-- Start of main section -->
+  <main>
+
     <h1>FORECAST</h1>
     <h3>COMBINING TASK MANAGEMENT WITH RESOURCING</h3>
+    <div id="banner">
     </div>
 
+  </main>
+<!-- End of main section -->
 
-</main>
+<!-- Start of footer -->
 
-  <!---Start of footer -->
-<hr />
-<footer>
+  <footer>
+    <hr />
     <div class="row">
-    <div class="col-sm-8 forecast">FORECAST - FORESEE ORGANISE RESOURCE EXCEL COLLABORATE ACHIEVE SHARE TRANSFER </div>
 
-  <div class="col-sm-4">
+      <div class="col-sm-8 forecast">FORECAST - FORESEE ORGANISE RESOURCE EXCHANGE COLLABORATE ACHIEVE SHARE TRANSFER </div>
+
+      <div class="col-sm-4">
         <p>Connect with us: <span class="addthis_inline_follow_toolbox connect"></span></p>
+      </div>
 
+    </div>
 
-        <!-- Go to www.addthis.com/dashboard to customize your tools -->
-  </div>
-  </div>
-
-
-
-        <nav class="navbar navbar-expand-sm" >
-            <span class="navbar-text">
-   &copy; 2019 Pauline Cairns
-  </span>
+    <!-- Bottom nav bar -->
+     <nav class="navbar navbar-expand-sm">
+        <span class="navbar-text">
+         &copy; 2019 Pauline Cairns
+        </span>
             <ul class="navbar-nav">
               <li class="nav-item"><a href="contact.html">Contact</a></li>
               <li class="nav-item"><a href="about.html">About</a></li>
               <li class="nav-item"><a href="t&c.html">T&Cs</a></li>
               <li class="nav-item"><a href="cookies.html">Cookies</a></li>
             </ul>
-        </nav>
+     </nav>
 
+  </footer>
+<!-- End of footer -->
 
-</footer>
+  <!-- End of <div class="container colour">-->
+  </div>
 
-</div>
-<!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5c853fcb8b82e462"></script>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <!--  www.addthis.com/dashboard script for connect tools -->
+  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5c853fcb8b82e462"></script>
+  <!-- Bootstrap script for general CSS layout-->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 </body>
+<!-- End main body-->
+
 </html>
 
 <?php
